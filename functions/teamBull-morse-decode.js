85008e846b12cbbde4344dfd7b60af48a3610958
Fernando de la Torre <fernandodelatorre4@gmail.com>
2022-09-29 17:16:44 +0200

    feat: implement 1st opt of decode

---
+++ functions/teamBull-morse-decode.js
@@ -0,0 +1,37 @@
+const morseDictionary = {
+    '.-': 'a',
+    '-...': 'b',
+    '-.-.': 'c',
+    '-..': 'd',
+    '.': 'e',
+    '..-.': 'f',
+    '--.': 'g',
+    '....': 'h',
+    '..': 'i',
+    '.---': 'j',
+    '-.-': 'k',
+    '.-..': 'l',
+    '--': 'm',
+    '-.': 'n',
+    '---': 'o',
+    '.--.': 'p',
+    '--.-': 'q',
+    '.-.': 'r',
+    '...': 's',
+    '-': 't',
+    '..-': 'u',
+    '...-': 'v',
+    '.--': 'w',
+    '-..-': 'x',
+    '-.--': 'y',
+    '--..': 'z',
+  };
+const translateLetter = (letter) => morseDictionary[letter];
+
+module.exports = misteryMessage =>
+    misteryMessage.split('   ').map(word =>{ 
+        const wordArr = word.split(' ');
+        const translatedWordArr = wordArr.map(translateLetter);
+        return translatedWordArr.join('');
+    }).join(' ');
+
