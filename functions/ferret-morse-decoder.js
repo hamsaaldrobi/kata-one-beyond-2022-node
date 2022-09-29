b211e1a67068062cc144eb065767030cb99b84d0
fernando.machuca <fernando.machuca@guidesmiths.com>
2022-09-29 17:24:38 +0200

    ferret-morse-decoder: initial push

---
+++ functions/ferret-morse-decoder.js
@@ -0,0 +1,57 @@
+const MORSE_CODE = {
+    ".-"   : "a",
+    "-..." : "b",
+    "-.-." : "c",
+    "-.."  : "d",
+    "."    : "e",
+    "..-." : "f",
+    "--."  : "g",
+    "...." : "h",
+    ".."   : "i",
+    ".---" : "j",
+    "-.-"  : "k",
+    ".-.." : "l",
+    "--"   : "m",
+    "-."   : "n",
+    "---"  : "o",
+    ".--." : "p",
+    "--.-" : "q",
+    ".-."  : "r",
+    "..."  : "s",
+    "-"    : "t",
+    "..-"  : "u",
+    "...-" : "v",
+    ".--"  : "w",
+    "-..-" : "x",
+    "-.--" : "y",
+    "--.." : "z",
+    ".----": "1",
+    "..---": "2",
+    "...--": "3",
+    "....-": "4",
+    ".....": "5",
+    "-....": "6",
+    "--...": "7",
+    "---..": "8",
+    "----.": "9",
+    "-----": "0",
+    "|"    : " "
+};
+
+const decode = (morseMessage) => {    
+    let messageDecoded = ''
+    let sentences = morseMessage.split('   ');
+    for (let i = 0; i < sentences.length; i++){
+        const word = sentences[i]
+        const words =  word.split(' ')
+        for (let x = 0; x < words.length; x++){
+        messageDecoded = messageDecoded + MORSE_CODE[words[x]];
+        }
+        messageDecoded = messageDecoded + ' '
+    }
+    return messageDecoded;    
+}
+
+module.exports = {
+    decode
+}
